8bf955a03074dca2ecae950c2c3d8e67c67dc781
Chris Spence <christopherspenceuk@outlook.com>
2020-03-15 09:30:22 +0000

    chore: update test files and poms

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,3 +1,4 @@
+
 package $package;
 
 public class Application {
--- src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -1,5 +1,17 @@
+
 package $package;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Application")
 public class ApplicationTest {
 
+  @Test
+  @DisplayName("Pointless test")
+  void smokeTest() {
+    assertThat(true).isEqualTo(true);
+  }
 }
